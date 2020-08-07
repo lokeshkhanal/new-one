c016a1d143a5508eef4bfc823a42410b3d12b3bc
lokesh khanal <lokesh.khanal88@gmail.com>
2020-08-07 16:23:27 +0545

    learning more

--- learn.js
+++ learn.js
@@ -63,14 +63,126 @@
 // console.log(biodatas);
 //     }
 // getdata();
-fetch('https://api.covid19api.com/summary').then((apidata)=>{
+// fetch('https://api.covid19api.com/summary').then((apidata)=>{
 
-console.log(apidata);
-return apidata.json();
-}).then((actualdata)=>{
-   // console.log(actualdata);
-const mydata=console.log(actualdata.Countries[118]);
-document.getElementById('data').innerHTML=`covid-19 details as ${mydata.NewConfirmed}`;
-}).catch((error)=>{
-console.log(`error is ${error}`);
-});
+// console.log(apidata);
+// return apidata.json();
+// }).then((actualdata)=>{
+//    // console.log(actualdata);
+// const datanp=(actualdata.Countries[118]);
+// //const mydata=datanp.map();
+// document.getElementById('data').innerHTML=`covid-19 details as NewConfirmed: ${datanp.NewConfirmed}
+// newdeath: ${datanp.NewDeaths}`;
+// }).catch((error)=>{
+// console.log(`error is ${error}`);
+// });
+//  async function getdata()
+// {
+//  const jsondata= await  fetch('https://api.covid19api.com/summary');
+// console.log(jsondata);
+// const jsdata=await jsondata.json();
+// console.log(jsdata.Countries[118]);
+// }
+// getdata();
+
+
+//tracker.
+var myform=document.getElementById("myform")
+myform.addEventListener('submit',(e)=>{
+   e.preventDefault();
+
+   var countrye=document.getElementById("country").value
+   console.log(countrye);
+   var cont=countrye.toLowerCase();
+
+   fetch('https://api.covid19api.com/summary').then((apidata)=>{
+      return apidata.json();
+
+   }
+      //console.log(ok);})
+      ).then((jsdata)=>{
+         console.log(jsdata);
+         const index=jsdata.Countries.length-1;console.log(index);
+       // console.log(actualinput.toString());
+        for(var i=index;i>=0;i--){
+         const lks=jsdata.Countries[index];//console.log(jsdata.Countries[index].NewConfirmed);
+        const actualinput=lks.Country.toLowerCase();//console.log(actualinput.toString());
+      //  console.log(actualinput.toString());
+      //  console.log(cont.toString());
+       
+       
+        if (actualinput.toString()==cont.toString())
+        {
+         const confirmed=document.getElementById('confirmed');
+         const recovered=document.getElementById('recovered');
+         const deaths=document.getElementById('deaths');
+         confirmed.append(jsdata.Countries[index].NewConfirmed);
+         recovered.append(jsdata.Countries[index].NewRecovered);
+         deaths.append(lksjsdata.Countries[index].NewDeaths);
+        }
+        }
+        
+
+
+      //   
+      //    console.log(jsdata.Countries[index]);
+   // var x = jsdata.map(country);
+   // console.log(x);
+
+  
+
+
+
+
+
+         // const length=ok.length;console.log(length);
+         // const index=length-1;
+         // console.log(index)
+         
+      })})
+     
+
+
+  
+
+// const getdata=async ()=>{
+//   const jsondata=await  fetch('https://api.covid19api.com/summary/'+country);
+//   const jsdata= await jsondata.json();console.log(jsdata)
+  
+// }
+// getdata();
+// })
+
+   // .then(()=>{
+   //    var length=data.length;
+   //    var index=length-1;
+   //    var confirmed=document.getElementById('confirmed');
+   //    var recovered=document.getElementById('recovered');
+   //    var deaths=document.getElementById('deaths');
+   //    confirmed.append(res[index].confirmed)
+ 
+   // })})
+
+
+// async function getdata()
+// {
+//  const jsondata= await  fetch('https://api.covid19api.com/summary');
+// //console.log(jsondata);
+// const jsdata=await jsondata.json();
+// //console.log(jsdata.Countries[118]);
+// //document.write(jsdata.Countries[118].country)
+// //console.log(jsdata.Countries);
+// const sjslength=jsdata.Countries.length;
+// console.log(sjslength);
+// const index=sjslength-1;
+// console.log(index);
+
+// console.log(document.getElementById("country"));
+
+
+// //document.getElementById("total confirmed cases:"+jsdata.)
+// }
+// getdata()
+
+
+
